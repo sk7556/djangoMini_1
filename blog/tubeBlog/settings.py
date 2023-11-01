bdffdba16d2a7827efc24219fd96f2365f960521
Jungsik <sk7556@naver.com>
2023-11-01 09:11:38 +0900

    임시 sqlite

--- blog/tubeBlog/settings.py
+++ blog/tubeBlog/settings.py
@@ -63,19 +63,10 @@ WSGI_APPLICATION = 'tubeBlog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tubedatabase',
-        'USER': 'masterJS',
-        'PASSWORD': 'simmani2',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
-
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
