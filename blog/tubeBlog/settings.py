d59617068d467abfd72f9ee1cec83824b56f4558
Jungsik <sk7556@naver.com>
2023-11-03 23:07:15 +0900

    postgre sql

--- blog/tubeBlog/settings.py
+++ blog/tubeBlog/settings.py
@@ -65,8 +65,12 @@ WSGI_APPLICATION = 'tubeBlog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tube',
+        'USER': 'jungsik',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
